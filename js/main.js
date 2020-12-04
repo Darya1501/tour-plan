769bf6b4884742413f13fbf979596c531bed836e
Darya1501 <darya_01@mail.ru>
2020-12-05 01:46:30 +0300

    Create: modal closes on press esc

--- js/main.js
+++ js/main.js
@@ -49,6 +49,8 @@ $(document).ready(function () {
   var closeModalButton = $(".modal__close");
   modalButton.on("click", openModal);
   closeModalButton.on("click", closeModal);
+  $(".modal__overlay").on("click", closeModal);
+  $(document).keyup(closeModal);
 
   function openModal() {
     var targetModal = $(this).attr("data-href");    
@@ -58,10 +60,12 @@ $(document).ready(function () {
 
   function closeModal(event) {
     event.preventDefault();
-    var modalOverlay = $(".modal__overlay");
-    var modalDialog = $(".modal__dialog");
-    modalOverlay.removeClass("modal__overlay--visible");
-    modalDialog.removeClass("modal__dialog--visible");
+    if (event.key === "Escape" || event.type === 'click') {
+        var modalOverlay = $(".modal__overlay");
+        var modalDialog = $(".modal__dialog");
+        modalOverlay.removeClass("modal__overlay--visible");
+        modalDialog.removeClass("modal__dialog--visible");
+    }
   }
 
 
